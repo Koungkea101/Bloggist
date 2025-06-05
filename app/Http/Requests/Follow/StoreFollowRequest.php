819bd90814a2d482e89f44ca6d0dd4fda720e6b8
Koungkea101 <nannkoungkea00@gmail.com>
2025-06-05 11:08:40 +0700

    feat: User create account, User Log in, add Not found page, create Follow API - write follow controller, follow requests and resource - connect authentication backend and frontend

--- app/Http/Requests/Follow/StoreFollowRequest.php
+++ app/Http/Requests/Follow/StoreFollowRequest.php
@@ -21,7 +21,7 @@ class StoreFollowRequest extends FormRequest
         //     return false;
         // }
 
-        return auth()->check();
+        return true;
     }
 
     /**
@@ -32,14 +32,15 @@ class StoreFollowRequest extends FormRequest
     public function rules(): array
     {
         return [
-          'following_id' => [
-            'required',
-            'exists:users,id',
-            'different:' . auth()->id(), // Ensure the user cannot follow themselves
-            Rule::unique('follows')->where(function ($query) {
-                return $query->where('follower_id', auth()->id());
-            }), // Ensure the user is not already following the other user
-          ]
+            'following_id' => [
+                'required',
+                'exists:users,id',
+                'different:' . auth()->id(), // Ensure the user cannot follow themselves
+                Rule::unique('follows')->where(function ($query) {
+                    return $query->where('follower_id', auth()->id())
+                                 ->where('following_id', $this->following_id);
+                }), // Ensure the user is not already following the other user
+            ]
         ];
     }
 
@@ -47,6 +48,9 @@ class StoreFollowRequest extends FormRequest
     public function messages(): array
     {
         return [
+            'follower_id.required' => 'Follower ID is required.',
+            'follower_id.exists' => 'The follower user does not exist.',
+            'following_id.required' => 'Following ID is required.',
             'following_id.different' => 'You cannot follow yourself.',
             'following_id.unique' => 'You are already following this user.',
             'following_id.exists' => 'The user you are trying to follow does not exist.',
